cdadb1e52efa0803f27fda98e7608a98fc032b65
fread007 <siansigsavourat@gmail.com>
2024-11-20 12:31:34 +0100

    more green line

--- stdes.c
+++ stdes.c
@@ -142,38 +142,41 @@ int vider(FICHIER *f){
 }
 
 int fecriref (FICHIER *f, const char *format, ...){
+    //ont verifie que les entree sont valide
     if (f == NULL || f->buffer == NULL || f->mode != 'E') {
         return 1;
     }
 
+    //initialisation des arguments
     va_list args;
     va_start(args, format);
 
     
-    int nbr_ecrit = 0;
-    char buffer[10];
+    int nbr_ecrit = 0;  //nombre d'element ecrit
+    char buffer[10];    //buffer pour transformer les int , la taille est de 10 car un int ne depasse pas 10 chiffres
 
+    //ont parcours la string format
     while(*format != '\0'){
-        if(*format == '%'){
-            format++;
+        if(*format == '%'){ //cas ou l'ont on a un element a inserer
+            format++;   //ont regarde qelle est le type de l'element
             switch(*format){
-                case 'c':
-                    char actuel = (char) va_arg(args, int);
-                    ecrire(&actuel, 1, 1, f);
+                case 'c':   //char
+                    char actuel = (char) va_arg(args, int);   //ont recupere l'element
+                    ecrire(&actuel, 1, 1, f);   //ont l'ecrit
                     nbr_ecrit++;
                     break;
-                case 's':
-                    char * string = va_arg( args, char * );
-                    while(*string != '\0'){
+                case 's':   //string
+                    char * string = va_arg( args, char * );  //ont recupere l'element
+                    while(*string != '\0'){   //ont ecrit chaque caractere de la string
                         ecrire(string, 1, 1, f);
                         nbr_ecrit++;
                         string++;
                     }
                     break;
-                case 'd':
-                    int i = va_arg(args, int);
-                    int length = int_to_string(i,buffer);
-                    ecrire(buffer, sizeof(char), length, f);
+                case 'd':   //int
+                    int i = va_arg(args, int);  //ont recupere l'element
+                    int length = int_to_string(i,buffer);   //ont transforme l'element en string
+                    ecrire(buffer, sizeof(char), length, f);    //ont l'ecrit
                     nbr_ecrit += length;
                     break;
                 default:
@@ -181,45 +184,50 @@ int fecriref (FICHIER *f, const char *format, ...){
             }
         }
         else{
+            //cas ou l'ont traite un caractere simple
             ecrire(format, sizeof(char), 1, f);
         }
         format++;
     }
  
+    //ont libere les arguments
     va_end(args);
     return nbr_ecrit;
 }
+
+
 /* directly in stdout */
 int ecriref (const char *format, ...){
 
+    //initialisation des arguments
     va_list args;
     va_start(args, format);
 
     
-    int nbr_ecrit = 0;
-    char buffer[10];
+    int nbr_ecrit = 0;  //nombre d'element ecrit
+    char buffer[10];    //buffer pour transformer les int , la taille est de 10 car un int ne depasse pas 10 chiffres
 
-    while(*format != '\0'){
-        if(*format == '%'){
+    while(*format != '\0'){ //ont parcours la string format
+        if(*format == '%'){ //cas ou l'ont on a un element a inserer
             format++;
-            switch(*format){
-                case 'c':
-                    char actuel = (char) va_arg(args, int);
-                    ecrire(&actuel, 1, 1, stdout);
+            switch(*format){    //ont regarde qelle est le type de l'element
+                case 'c':   //char
+                    char actuel = (char) va_arg(args, int);  //ont recupere l'element
+                    ecrire(&actuel, 1, 1, stdout);  //ont l'ecrit
                     nbr_ecrit++;
                     break;
-                case 's':
-                    char * string = va_arg( args, char * );
-                    while(*string != '\0'){
-                        ecrire(string, 1, 1, stdout);
-                        nbr_ecrit++;
+                case 's':   //string
+                    char * string = va_arg( args, char * ); //ont recupere l'element
+                    while(*string != '\0'){   //ont ecrit chaque caractere de la string
+                        ecrire(string, 1, 1, stdout);   //ont l'ecrit
+                        nbr_ecrit++;    
                         string++;
                     }
                     break;
-                case 'd':
-                    int i = va_arg(args, int);
-                    int length = int_to_string(i,buffer);
-                    ecrire(buffer, sizeof(char), length, stdout);
+                case 'd':   //int
+                    int i = va_arg(args, int);  //ont recupere l'element
+                    int length = int_to_string(i,buffer);   //ont transforme l'element en string
+                    ecrire(buffer, sizeof(char), length, stdout);   //ont l'ecrit
                     nbr_ecrit += length;
                     break;
                 default:
@@ -227,11 +235,13 @@ int ecriref (const char *format, ...){
             }
         }
         else{
+            //cas ou l'ont traite un caractere simple
             ecrire(format, sizeof(char), 1, stdout);
         }
         format++;
     }
 
+    //ont libere les arguments
     va_end(args);
     return nbr_ecrit;
 }
@@ -239,72 +249,76 @@ int ecriref (const char *format, ...){
 
 int fliref (FICHIER *f, const char *format, ...) {
     
+    if(f == NULL || f->buffer == NULL || f->mode != 'L'){   //ont verifie que les entree sont valide
+        return 0;
+    }
+
+    //initialisation des arguments
     va_list args;
     va_start(args, format);
 
     
-    int nbr_lecture = 0;
-    char tmp;
-    int lu = -1;
-    int flag = 0;
+    int nbr_lecture = 0;    //nombre d'element lu
+    char tmp;   
+    int lu = -1;    //permet de savoir si l'ont a lu un caractere
+    int flag = 0;   //permer de savoir si ont a lu un caractere en plus
 
-    while(*format != '\0'){
-        if(*format == '%'){
+    while(*format != '\0'){ //ont parcours la string format
+        if(*format == '%'){ //cas ou l'ont on a un element a recuperer
             format++;
-            switch(*format){
-                case 'c' :
-                    char *c = va_arg(args, char *);
-                    if(flag == 1){
+            switch(*format){    //ont regarde qelle est le type de l'element
+                case 'c' :  //char
+                    char *c = va_arg(args, char *); //ont recupere l'emplacement ou ont doit inserer l'element
+                    if(flag == 1){  //si l'ont a deja lu le caractere ont l'insere
                         *c = tmp;
                         flag = 0;
                     }
-                    else if(lire(&tmp, 1, 1, f) == 1){
+                    else if(lire(&tmp, 1, 1, f) == 1){  //sinon ont le lit
                         nbr_lecture++;
                     }
-                    else {
+                    else {  //si il na pas pu etre lu ont arrte
                         return nbr_lecture;
                     }
                 break;
-                case 's' :
-                    vider(stdout);
-                    char *s = va_arg(args, char *);
-                    int i = 0;
+                case 's' :  //string
+                    char *s = va_arg(args, char *); //ont recupere l'emplacement ou ont doit inserer l'element
+                    int i = 0;  //index du tableau
                     lu = -1;
-                    if(flag == 1 && tmp != ' ' && tmp != '\n' && tmp != '\0' && tmp != '\t'){
+                    if(flag == 1 && tmp != ' ' && tmp != '\n' && tmp != '\0' && tmp != '\t'){   //si l'ont a deja lu un caractere ont l'insere
                         s[i] = tmp;
                         i++;
                     }
-                    while((lu=lire(&tmp, 1, 1, f)) == 1 && tmp != ' ' && tmp != '\n' && tmp != '\0' && tmp != '\t'){
+                    while((lu=lire(&tmp, 1, 1, f)) == 1 && tmp != ' ' && tmp != '\n' && tmp != '\0' && tmp != '\t'){    //ont li les caracetere jusqu'a un separateur
                         s[i] = tmp;
                         i++;
                     }
-                    s[i] = '\0';
+                    s[i] = '\0';    //ont ajoute le caractere de fin de string
                     nbr_lecture++;
-                    if(lu == 0 || *format == '\0'){
+                    if(lu == 0 || *format == '\0'){ //si ont est a la fin du fichier ou de la string ont arrete
                         return nbr_lecture;
                     }
-                    flag = 1;
+                    flag = 1;   //ont indique que l'ont a lu un caractere en plus
                 break;
-                case 'd' :
-                    int *integer = va_arg(args, int *);
-                    char buffer[10];
+                case 'd' :  //int
+                    int *integer = va_arg(args, int *);  //ont recupere l'emplacement ou ont doit inserer l'element
+                    char buffer[11];    //buffer pour transformer les int , la taille est de 11 car un int ne depasse pas 10 chiffres et il faut un caractere de fin de string
                     lu = -1;
                     int ctr = 0;
-                    if(flag == 1 && tmp <= '9' && tmp >= '0'){
+                    if(flag == 1 && tmp <= '9' && tmp >= '0'){  //si l'ont a deja lu un caracterent et que c'est un chiffre ont l'insere
                         buffer[ctr] = tmp;
                         ctr++;
                     }
-                    while((lu = lire(&tmp, 1, 1, f)) == 1 &&  tmp <= '9' && tmp >= '0'){
+                    while((lu = lire(&tmp, 1, 1, f)) == 1 &&  tmp <= '9' && tmp >= '0'){    //ont li les chiffres
                         buffer[ctr] = tmp;
                         ctr++;
                     }
-                    buffer[ctr] = '\0';
-                    *integer = atoi(buffer);
+                    buffer[ctr] = '\0'; //ont ajoute le caractere de fin de string
+                    *integer = atoi(buffer);    //ont transforme la string en int
                     nbr_lecture++;
-                    if(lu == 0 || *format == '\0'){
+                    if(lu == 0 || *format == '\0'){ //si ont est a la fin du fichier ou de la string ont arrete
                         return nbr_lecture;
                     }
-                    flag = 1;
+                    flag = 1;   //ont indique que l'ont a lu un caractere en plus
                     
                 break;
                 default:
@@ -313,13 +327,14 @@ int fliref (FICHIER *f, const char *format, ...) {
             }
         }
         else{
-            if(flag == 1){
+            //cas ou l'ont traite un caractere simple
+            if(flag == 1){  //si on a deja lu un caractere 
                 flag = 0;
             }
-            else{
+            else{   //sinon ont lit le caractere
                 lire(&tmp, 1, 1, f);
             }
-            if(tmp != *format){
+            if(tmp != *format){ //ont verifie que le caractere lu est bien celui attendu
                 return nbr_lecture;
             }
         }
@@ -327,7 +342,7 @@ int fliref (FICHIER *f, const char *format, ...) {
         format++;
     }
 
-
+    //ont libere les arguments
     va_end(args);
     return nbr_lecture;
     
